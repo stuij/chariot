9d176f3a26f842237165a7cbb979ee2b32323333
Ties Stuij <ties@stuij.se>
2008-06-05 10:01:00 +0000

    automatically insert lit when finding nr literal in defword
    darcs-hash:20080605100100-f86be-90927c4b5a201128e1da8a7e56bf83125d789a82

--- src/words.lisp
+++ src/words.lisp
@@ -28,7 +28,10 @@
 
 (defmacro defword (name more-params &body words)
   (let ((word-list (loop for word in words
-                      collect `(word (address ,(intern (symbol-name word) :keyword))))))
+                      append (etypecase word 
+                                (symbol `((word (address ,(intern (symbol-name word) :keyword)))))
+                                (number `((word (address :lit))
+                                          (word ,word)))))))
     `(defword-builder ,name ,more-params
        (word (address :%docol))
        ,@word-list
