563ab2262441c43be4ca75334910ad3d161b53c2
Ties Stuij <ties@stuij.se>
2008-06-12 10:12:16 +0000

    evolved the make-eval-forth thing
    we now wired in the liards libraries, and font data, and now initialize the system, and as a test, write a text to the screen before jumping to forth
    
    darcs-hash:20080612101216-f86be-d8c5a74d7d0a072e61729fcd082894f1bb335d95

--- src/start.lisp
+++ src/start.lisp
@@ -71,58 +71,55 @@
 
 (set-asm-init-routines
   (emit-asm
+   
    ;; var to hold the last link address in the word chain
    (def-asm-param link 0)
-
    (b :init-at-end)))
 
-(defun make-eval-forth (string)
-  (setf *simul-input* string)
-  (set-asm-final-routines
-    (emit-asm
+(defun make-eval-forth (&optional fth-file)
+  (let ((fth-content (strcat (read-string-from-file (concatenate 'string (namestring *forth-source-dir*) "/"  "lib.fth") :external-format :latin-1)
+                             " "
+                             (when fth-file
+                               (read-string-from-file (concatenate 'string (namestring *forth-source-dir*) "/"  fth-file) :external-format :latin-1))))
+        (liards::*letter-spacing* 1))
+    (liards::make-font)
+    (set-asm-final-routines
+      (emit-asm
+       
+       :init-at-end
+       ;; SETUP CODE
 
-     :init-at-end
-     ;; SETUP CODE
-     ;; setting up the regs we need to set up
-     (ldr sb *ps-base*)
-     (mov sp sb)
-     (ldr tib *tib-base*)
-     (ldr rb *rs-base*)
-     (mov rp rb)
+       ;; init screens       
+       (b-and-l :init-system)
 
-     ;; setting up user-vars
-     ;; latest
-     (ldr tmp-1 (address :latest-var))
-     (ldr tmp-2 (ia link))
-     (str tmp-2 (tmp-1))
-     ;; here
-     (ldr tmp-1 (address :here-var))
-     (ldr tmp-2 (address :code-end))
-     (add tmp-2 tmp-2 4) ;; skip past the eternal loop that is now the arm7 code. this is a hack 
-     (str tmp-2 (tmp-1))
+       ;; setting up the regs we need to set up
+       (ldr sb *ps-base*)
+       (mov sp sb)
+       (ldr tib *tib-base*)
+       (ldr rb *rs-base*)
+       (ldr jr (address :jr-base))
+       (mov rp rb)
 
-     ;; put ip at beginning of simulated word
-     (ldr ip (address :ip-start))
-     next
+       ;; setting up user-vars
+       ;; latest
+       (ldr tmp-1 (address :latest-var))
+       (ldr tmp-2 (ia link))
+       (str tmp-2 (tmp-1))
 
-     ;; SETUP DATA   
+       ;; here
+       (ldr tmp-1 (address :here-var))
+       (ldr tmp-2 (address :code-end))
+       (add tmp-2 tmp-2 4) ;; skip past the eternal loop that is now the arm7 code. this is a hack 
+       (str tmp-2 (tmp-1))
 
-     pool ;; just to be sure. perhaps the tmp tib becomes to big for ldr to fetch it's data
+       ;; put ip at beginning of simulated word
+       (ldr ip (address :ip-start))
 
-     ;; start of ip
-     :ip-start
-     (word (address :quit))
+       (b-and-l :write-test-string)
 
-     ;; temporary hackish tib-base. For now functions at uninteractive input prompt
-     :tib-base
-     (ea *simul-input*)
-     " ETERNAL " ;; so we don't have to think about appending a space to get a valid last input word
-     :tib-top
-     align))
-  (liards::nds-compile
-   (assemble 'arm9 'arm (emit-arm-fns))
-   liards::*arm7-bin*
-   "fuck-around.nds"))
+       ;; enter the forth interpret loop
+       next
+       
 
        ;; SETUP DATA
        
