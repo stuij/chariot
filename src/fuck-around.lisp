81265afd0767d1b500bab92cc8384a2d46101c64
Ties Stuij <ties@stuij.se>
2008-06-17 11:44:03 +0000

    moved the infamous turtle line to fuck-around. should really make it an in-built function
    darcs-hash:20080617114403-f86be-c799a08ad1f2cacb9f8f657edc6fd6d6d49dacfc

--- src/fuck-around.lisp
+++ src/fuck-around.lisp
@@ -8,7 +8,7 @@
 
 ;; *current-asm-block*
 
-;; (assemble 'arm9 'arm '((and tmp-4 tmp-4 tmp-5)))
+;; (assemble 'arm9 'arm `((add r1 r1 4 :lsl 1)))
 
 ;; (clear-current-block)
 
@@ -22,5 +22,33 @@
   ;; word find break >cfa
   ;; lit won't be automatically prepended
   )
+
+(let* ((string "\"And so you see, in each moment you must be catching up the distance between us, and yet I, at the same time, will be adding a new distance, however small, for you to catch up again.\"
+
+\"Indeed, it must be so,\" said Achilles wearily.
+
+\"And so you can never catch up,\" the Tortoise concluded sympathetically.
+
+\"You are right, as always,\" said Achilles sadly, and conceded the race.
+but then he thought..")
+       (string-length (length string)))
+
+  (defcode test-write ()
+    (ldr tmp-2 (address :test-string))
+    (push-ps tmp-2)
+    (ldr tmp-1 string-length)
+    (push-ps tmp-1)
+    (b-and-l :write-string))
+
+  (def-asm-fn some-test-strings
+    :test-string
+    (ea string)
+    align
+    pool))
+
+(defword test-string ()
+  break litstring 3 "bla " tell)
+
+*jr-reachables*
 
 (make-eval-forth "test.fth")
